71b88578b8ae7ada0d0525a59364885d46a365bf
pezerafc <pezer.tomislav@gmail.com>
2021-12-08 00:35:46 +0100

    second commit- added dynamic data + connected db

--- app.js
+++ app.js
@@ -1,33 +1,32 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog')
+
 
 //express app
 const app = express();
 
+
+//connect to mongodb
+const dbURI = 'mongodb+srv://pezerafc:test123@blog-app.oygeg.mongodb.net/blog-app?retryWrites=true&w=majority';
+mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology:true})
+    .then((result)=> app.listen(3000))
+        .catch((err) => console.log(err));
 //register view engine
 
 app.set('view engine', 'ejs');
 
 
-//listen for requests
-app.listen(3000);
-
-
 //middleware & static files(css,slike itd)
-app.use(express.static('public')) /
-
+app.use(express.static('public'))
 app.use(morgan('dev'))
+
 
 
 //routing & html
 app.get('/', (req,res) =>{
-const blogs = [
-    {title: 'First blog title', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'Second blog title', snippet: 'Nulla vero ullam reiciendis dicta sequi'},
-    {title: 'Third blog title', snippet: 'quaerat impedit sit laudantium voluptatem'}
-];
-
-    res.render('index', {title: 'Home', blogs}); //view engine treba za dynamic data(html je u ejs dokumentu)
+    res.redirect('/blogs');
     
     
     //res.sendFile('./views/index.html', {root: __dirname}); bez view engine
@@ -39,17 +38,31 @@ app.get('/about', (req,res) =>{
     //res.send('<p>about page</p>')
 })
 
+
+//blog routes
+app.get('/blogs', (req,res)=>{
+    Blog.find().sort({createdAt: -1})
+        .then((result)=>{
+            res.render('index', {title: 'All Blogs', blogs: result})
+        })
+        .catch(err=>{
+            console.log(err);
+        })
+})
+
 app.get('/blogs/create', (req,res)=>{
     res.render('create', {title: 'Create'})
 })
 
 //redirects
 // app.get('/about-us', (req,res) => {
-//     res.redirect('/about')
-// })
-
-
-//404 page
-app.use((req,res)=>{
-    res.status(404).render('404', {title: '404'})
-})
+    //     res.redirect('/about')
+    // })
+    
+    
+    //404 page
+    app.use((req,res)=>{
+        res.status(404).render('404', {title: '404'})
+    })
+    
+    
